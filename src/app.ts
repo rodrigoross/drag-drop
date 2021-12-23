f247c46491c27d3ba5c6bd9d7f2685899f744191
rodrigoross <rodrigo.ross.comp@gmail.com>
2021-12-23 15:22:29 -0400

    chore(decorator): criado bind com ProjectInput

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
 /**
+ * Autobind Decorator,  como método decorator
+ */
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value; // acessa o método original
+  // Cria um novo descritor para passar para o método
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this); // Dessa forma o this é feito o bind automatico.
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+
+/**
  * OOP
+ * Project Input Class
  */
 class ProjectInput {
   templateElement: HTMLTemplateElement;
@@ -59,14 +78,16 @@ class ProjectInput {
    * Método responsável por adicionar listener no submit do formulario.
    */
   private configure() {
-    //   this.formElement.addEventListener('submit', this.handleSubmit); // This. que será executado no evento não se refere a classe instanciada.
-      this.formElement.addEventListener('submit', this.handleSubmit.bind(this)); // Agora é repassado o mesmo this, executado no método configure (class ProjectInput) para o metodo do submit
+    // this.formElement.addEventListener('submit', this.handleSubmit); // This. que será executado no evento não se refere a classe instanciada.
+    // this.formElement.addEventListener('submit', this.handleSubmit.bind(this)); // Agora é repassado o mesmo this, executado no método configure (class ProjectInput) para o metodo do submit
+    this.formElement.addEventListener("submit", this.handleSubmit)
   }
 
   /**
    * Método responsável por executar a lógica de submit.
    * @param event 
    */
+  @Autobind
   private handleSubmit(event: Event) {
     event.preventDefault();
 
