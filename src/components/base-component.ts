756cadd31fb64fa56f652ed2a1c4c8ac9a10036e
rodrigoross <rodrigo.ross.comp@gmail.com>
2021-12-28 14:03:11 -0400

    refactor(modules): altera para import/export do es6
    Remove o uso de Namespaces para utilizad Import/Export do ES6 Modules

--- src/components/base-component.ts
+++ src/components/base-component.ts
@@ -3,50 +3,48 @@
  *
  * Usa Generics visto que o host e element pode ser qqer tipo de elemento dom
  */
-namespace App {
-  export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
-    templateElement: HTMLTemplateElement;
-    hostElement: T; // Onde o elemento será renderizado
-    element: U; // Qual Elemento será renderizado
+export abstract class Component<T extends HTMLElement, U extends HTMLElement> {
+  templateElement: HTMLTemplateElement;
+  hostElement: T; // Onde o elemento será renderizado
+  element: U; // Qual Elemento será renderizado
 
-    constructor(
-      templateId: string,
-      hostElementId: string,
-      insertAtStart: boolean,
-      newElementId?: string
-    ) {
-      // Busca elemento com o template do formulario
-      this.templateElement = document.getElementById(
-        templateId
-      )! as HTMLTemplateElement;
+  constructor(
+    templateId: string,
+    hostElementId: string,
+    insertAtStart: boolean,
+    newElementId?: string
+  ) {
+    // Busca elemento com o template do formulario
+    this.templateElement = document.getElementById(
+      templateId
+    )! as HTMLTemplateElement;
 
-      // Div onde a aplicação será executada.
-      this.hostElement = document.getElementById(hostElementId)! as T;
+    // Div onde a aplicação será executada.
+    this.hostElement = document.getElementById(hostElementId)! as T;
 
-      //Carrega o conteudo do template para renderizar no DOM.
-      const importContent = document.importNode(
-        this.templateElement.content,
-        true
-      );
+    //Carrega o conteudo do template para renderizar no DOM.
+    const importContent = document.importNode(
+      this.templateElement.content,
+      true
+    );
 
-      // Pega o primeiro elemento (form) e salva na váriavel
-      this.element = importContent.firstElementChild as U;
-      if (newElementId) this.element.id = newElementId; // adiciona ID para aplicar CSS e identificar
+    // Pega o primeiro elemento (form) e salva na váriavel
+    this.element = importContent.firstElementChild as U;
+    if (newElementId) this.element.id = newElementId; // adiciona ID para aplicar CSS e identificar
 
-      this.attach(insertAtStart);
-    }
-
-    /**
-     * Método responsável por adicionar conteuod do DOM no HTML.
-     */
-    private attach(insertAtBeginning: boolean) {
-      this.hostElement.insertAdjacentElement(
-        insertAtBeginning ? "afterbegin" : "beforeend",
-        this.element
-      );
-    }
-
-    abstract configure(): void;
-    abstract renderContent(): void;
+    this.attach(insertAtStart);
   }
+
+  /**
+   * Método responsável por adicionar conteuod do DOM no HTML.
+   */
+  private attach(insertAtBeginning: boolean) {
+    this.hostElement.insertAdjacentElement(
+      insertAtBeginning ? "afterbegin" : "beforeend",
+      this.element
+    );
+  }
+
+  abstract configure(): void;
+  abstract renderContent(): void;
 }
--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,52 +1,50 @@
-/// <reference path="base-component.ts" />
-/// <reference path="../decorators/autobind.ts" />
-/// <reference path="../models/project.ts" />
-/// <reference path="../models/drag-drop.ts" />
+import { Component } from "../components/base-component.js";
+import { Project } from "../models/project.js";
+import { Autobind } from "../decorators/autobind.js";
+import { Draggable } from "../models/drag-drop.js";
 
 /**
  * Project Item
  */
-namespace App {
-  export class ProjectItem
-    extends Component<HTMLUListElement, HTMLLIElement>
-    implements Draggable
-  {
-    private projeto: Project;
+export class ProjectItem
+  extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable
+{
+  private projeto: Project;
 
-    get pessoas() {
-      if (this.projeto.pessoas === 1) {
-        return "1 pessoa";
-      } else {
-        return `${this.projeto.pessoas} pessoas`;
-      }
+  get pessoas() {
+    if (this.projeto.pessoas === 1) {
+      return "1 pessoa";
+    } else {
+      return `${this.projeto.pessoas} pessoas`;
     }
+  }
 
-    constructor(hostId: string, project: Project) {
-      super("single-project", hostId, false, project.id);
-      this.projeto = project;
+  constructor(hostId: string, project: Project) {
+    super("single-project", hostId, false, project.id);
+    this.projeto = project;
 
-      this.configure();
-      this.renderContent();
-    }
+    this.configure();
+    this.renderContent();
+  }
 
-    @Autobind
-    dragStartHandler(event: DragEvent) {
-      event.dataTransfer!.setData("text/plain", this.projeto.id); // Transfere somente o ID para poder recuper posteriormente
-      event.dataTransfer!.effectAllowed = "move";
-    }
+  @Autobind
+  dragStartHandler(event: DragEvent) {
+    event.dataTransfer!.setData("text/plain", this.projeto.id); // Transfere somente o ID para poder recuper posteriormente
+    event.dataTransfer!.effectAllowed = "move";
+  }
 
-    dragEndHandler(_: DragEvent) {}
+  dragEndHandler(_: DragEvent) {}
 
-    configure() {
-      this.element.addEventListener("dragstart", this.dragStartHandler);
-      this.element.addEventListener("dragend", this.dragEndHandler);
-    }
+  configure() {
+    this.element.addEventListener("dragstart", this.dragStartHandler);
+    this.element.addEventListener("dragend", this.dragEndHandler);
+  }
 
-    renderContent() {
-      this.element.querySelector("h2")!.textContent = this.projeto.titulo;
-      this.element.querySelector("h3")!.textContent =
-        this.pessoas + " no projeto";
-      this.element.querySelector("p")!.textContent = this.projeto.descricao;
-    }
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.projeto.titulo;
+    this.element.querySelector("h3")!.textContent =
+      this.pessoas + " no projeto";
+    this.element.querySelector("p")!.textContent = this.projeto.descricao;
   }
 }
